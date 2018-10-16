c559a98ad67187d2891d0cc28577bf755ac8fbc8
Heaven <441339905@qq.com>
2018-10-16 13:37:08 +0800

    upload file

--- microservisecloud-consumer-dept-80/src/main/java/com/zhanghf/springcloud/controller/DeptController_Consumer.java
+++ microservisecloud-consumer-dept-80/src/main/java/com/zhanghf/springcloud/controller/DeptController_Consumer.java
@@ -18,6 +18,7 @@ public class DeptController_Consumer {
 //    private static final String REST_URL_PREFIX = "http://localhost:8001";
       private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
 
+
     @Autowired
     private RestTemplate restTemplate;
 
